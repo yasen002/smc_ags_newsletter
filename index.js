42b03a9ccc8bd8821350bcb7f780d1741bb279da
yasen002 <46136113+yasen002@users.noreply.github.com>
2022-05-02 17:13:17 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,303 @@
+const data = {
+  "2021_2022": [
+    {
+      issueNumber: 1,
+      editor: "Clarissa Sherine Widjaja",
+      date: "11/18/2021",
+      pages: "20",
+      url: "",
+    },
+    {
+      issueNumber: 2,
+      editor: "Clarissa Sherine Widjaja",
+      date: "11/18/2021",
+      pages: "20",
+      url: "",
+    },
+    {
+      issueNumber: 3,
+      editor: "Clarissa Sherine Widjaja",
+      date: "12/30/2021",
+      pages: "24",
+      url: "",
+    },
+  ],
+  "2020_2021": [
+    {
+      issueNumber: 1,
+      editor: "Sooleen Kim",
+      date: "11/18/2020",
+      pages: "20",
+      url: "",
+    },
+    {
+      issueNumber: 2,
+      editor: "Kristivera Vidinati",
+      date: "03/15/2021",
+      pages: "9",
+      url: "",
+    },
+    {
+      issueNumber: 3,
+      editor: "Kristivera Divinati",
+      date: "04/07/2021",
+      pages: "12",
+      url: "",
+    },
+    {
+      issueNumber: 4,
+      editor: "Kristivera Divinati",
+      date: "07/01/2021",
+      pages: "34",
+      url: "",
+    },
+  ],
+  "2019_2020": [
+    {
+      issueNumber: 1,
+      editor: "Natasha Grace",
+      date: "10/17/2019",
+      pages: "12",
+      url: "",
+    },
+    {
+      issueNumber: 2,
+      editor: "Natasha Grace",
+      date: "11/14/2019",
+      pages: "12",
+      url: "",
+    },
+    {
+      issueNumber: 3,
+      editor: "Natasha Grace",
+      date: "12/5/2019",
+      pages: "12",
+      url: "",
+    },
+    {
+      issueNumber: 4,
+      editor: "Kim Hwang yeo",
+      date: "05/27/2020",
+      pages: "20",
+      url: "",
+    },
+  ],
+  "2018_2019": [
+    {
+      issueNumber: 1,
+      editor: "Ariana Kalantari",
+      date: "11/15/2018",
+      pages: "12",
+      url: "",
+    },
+    {
+      issueNumber: 2,
+      editor: "Ariana Kalantari",
+      date: "12/06/2019",
+      pages: "12",
+      url: "",
+    },
+    {
+      issueNumber: 3,
+      editor: "Ariana Kalantari",
+      date: "04/04/2019",
+      pages: "12",
+      url: "",
+    },
+    {
+      issueNumber: 4,
+      editor: "Ariana Kalantari",
+      date: "05/02/2019",
+      pages: "12",
+      url: "",
+    },
+  ],
+
+  "2017_2018": [
+    {
+      issueNumber: 1,
+      editor: "Thomas Hoversen",
+      date: "11/02/2017",
+      pages: "8",
+      url: "",
+    },
+    {
+      issueNumber: 2,
+      editor: "Thomas Hoversen",
+      date: "12/07/2017",
+      pages: "8",
+      url: "",
+    },
+    {
+      issueNumber: 3,
+      editor: "Thomas Hoversen",
+      date: "03/22/2017",
+      pages: "7",
+      url: "",
+    },
+    {
+      issueNumber: 4,
+      editor: "Thomas Hoversen",
+      date: "04/05/2018",
+      pages: "12",
+      url: "",
+    },
+  ],
+
+  "2015_2016": [
+    {
+      issueNumber: 1,
+      editor: "Elizabeth King",
+      date: "10/08/2015",
+      pages: "16",
+      url: "",
+    },
+    {
+      issueNumber: 2,
+      editor: "Elizabeth King",
+      date: "11/05/2015",
+      pages: "16",
+      url: "",
+    },
+    {
+      issueNumber: 3,
+      editor: "Elizabeth King",
+      date: "12/10/2015",
+      pages: "16",
+      url: "",
+    },
+    {
+      issueNumber: 4,
+      editor: "Elizabeth King",
+      date: "04/07/2016",
+      pages: "12",
+      url: "",
+    },
+    {
+      issueNumber: 5,
+      editor: "Elizabeth King",
+      date: "04/12/2016",
+      pages: "8",
+      url: "",
+    },
+    {
+      issueNumber: 6,
+      editor: "Elizabeth King",
+      date: "06/02/2016",
+      pages: "8",
+      url: "",
+    },
+  ],
+  "2014_2015": [
+    {
+      issueNumber: 1,
+      editor: "Evelyn Huynh",
+      date: "10/16/2014",
+      pages: "12",
+      url: "",
+    },
+    {
+      issueNumber: 2,
+      editor: "Evelyn Huynh",
+      date: "11/06/2014",
+      pages: "12",
+      url: "",
+    },
+    {
+      issueNumber: 3,
+      editor: "Evelyn Huynh",
+      date: "12/04/2014",
+      pages: "12",
+      url: "",
+    },
+    {
+      issueNumber: 4,
+      editor: "Alana Rivera",
+      date: "12/04/2014",
+      pages: "12",
+      url: "https://drive.google.com/file/d/135_FrQCjuWFg5qM-HN65_3dZr3z0-daP/view",
+    },
+  ],
+  "2013_2014": [
+    {
+      issueNumber: 1,
+      date: "10.17.2013",
+      pages: 7,
+      editor: "Jonathan Canahui",
+      url: "https://drive.google.com/file/d/1JqqC0bDOCJlkO1QA8GkRfwndhDXOFHTh/view",
+    },
+    {
+      issueNumber: 2,
+      date: "11.14.2013",
+      pages: 7,
+      editor: "Jonathan Canahui",
+      url: "https://drive.google.com/file/d/1JqqC0bDOCJlkO1QA8GkRfwndhDXOFHTh/view",
+    },
+    {
+      issueNumber: 3,
+      date: "12.05.2013",
+      pages: 7,
+      editor: "Jonathan Canahui",
+      url: "https://drive.google.com/file/d/1JqqC0bDOCJlkO1QA8GkRfwndhDXOFHTh/view",
+    },
+  ],
+  "2012_2013": [
+    {
+      issueNumber: 1,
+      date: "10.04.2012",
+      pages: 12,
+      editor: "Sierra Mcdonald",
+      url: "https://drive.google.com/file/d/1JqqC0bDOCJlkO1QA8GkRfwndhDXOFHTh/view",
+    },
+    {
+      issueNumber: 2,
+      date: "11.08.2012",
+      pages: 12,
+      editor: "Sierra Mcdonald",
+      url: "https://drive.google.com/file/d/1JqqC0bDOCJlkO1QA8GkRfwndhDXOFHTh/view",
+    },
+    {
+      issueNumber: 3,
+      date: "12.18.2012",
+      pages: 12,
+      editor: "Sierra Mcdonald",
+      url: "https://drive.google.com/file/d/1JqqC0bDOCJlkO1QA8GkRfwndhDXOFHTh/view",
+    },
+    {
+      issueNumber: 4,
+      date: "04.04.2013",
+      pages: 12,
+      editor: "Jonathan Canahui",
+      url: "https://drive.google.com/file/d/1JqqC0bDOCJlkO1QA8GkRfwndhDXOFHTh/view",
+    },
+  ],
+};
+
+function addElement() {
+  // create a new div element
+  const newDiv = document.createElement("div");
+
+  // and give it some content
+  const newContent = document.createTextNode("Hi there and greetings!");
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("newsletters1");
+  document.body.appendChild(newDiv, currentDiv);
+}
+
+addElement();
+
+//selecting the parent. where we want to insert the html element
+var news = document.getElementsByClassName("news-story")[0];
+
+for (var i = 0; i < data.length; i++) {
+  var h5 = document.createElement("h5");
+  h5.innerHTML = data[i].title;
+  news.appendChild(h5);
+  var p = document.createElement("p");
+  p.innerHTML = data[i].author;
+  news.appendChild(p);
+}
